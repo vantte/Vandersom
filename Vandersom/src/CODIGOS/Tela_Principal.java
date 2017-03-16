7a2561308182dd7f700e3f72ebbc305c9e5e36a4
corag <corag@DESKTOP-DBC7OJO>
2017-03-16 12:57:51 -0300

    3

--- Vandersom/src/CODIGOS/Tela_Principal.java
+++ Vandersom/src/CODIGOS/Tela_Principal.java
@@ -84,7 +84,7 @@ public class Tela_Principal extends javax.swing.JFrame {
 
     private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
         // TODO add your handling code here:
-        Tela_Controle_de_Turmas t = new Tela_Controle_de_Turmas();
+        Tela_Interna t = new Tela_Interna();
         jDesktopPane1.add(t);
         t.setVisible(true);
     }//GEN-LAST:event_jMenuItem1ActionPerformed
